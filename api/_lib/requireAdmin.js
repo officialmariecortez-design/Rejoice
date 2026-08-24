49bfe2d259750107b17586d79644340dd31e391f
officialmariecortez-design <officialmariecortez@gmail.com>
2026-08-24 10:58:55 -0300

    Delete api directory

--- api/_lib/requireAdmin.js
+++
@@ -1,8 +0,0 @@
-export function isAdmin(req) {
-  const key = req.headers['x-admin-key']
-  return Boolean(process.env.ADMIN_API_KEY) && key === process.env.ADMIN_API_KEY
-}
-
-export function unauthorized(res) {
-  res.status(401).json({ error: 'Unauthorized.' })
-}
